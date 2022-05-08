9326c2e1db284f7750f4dae7a61eb5b867cf84ad
Rishith18 <69175269+Rishith18@users.noreply.github.com>
2022-05-08 11:37:26 -0400

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,20 @@
+def swapFileData():
+    file1="sample1.txt"
+
+    file2="sample2.txt"
+
+    a = open(file1, 'r')
+    data_a = a.read()
+
+    b = open(file2, 'r')
+    data_b = b.read()
+
+    with open(file1, 'w') as a:
+        a.write(data_b)
+
+    with open(file2, 'w') as b:
+        b.write(data_a)
+
+    print(data_a)
+
+swapFileData()
